d8da4920bbc4fd1de4a934594bcef8219390a27a
Nethmi Rashmika <h.u.g.nethmirashmika@gmail.com>
2026-06-14 22:59:09 +0530

    Initial commit: project setup with login tests

---
+++ src/main/java/com/orangehrm/orangehrm_automation/OrangehrmAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.orangehrm.orangehrm_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangehrmAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangehrmAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangehrm/orangehrm_automation/OrangehrmAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangehrm.orangehrm_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangehrmAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
